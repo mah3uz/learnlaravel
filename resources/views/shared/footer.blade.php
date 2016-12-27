e5577f68b2a8082cf87436cf95bb40d69fa411a4
Mahfuz Shaikh <jacosshaikh@gmail.com>
2016-12-27 16:58:11 +0600

    My Website

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,18 @@
+<footer id="footer" style="background: rgba(251, 89, 70, 0.64); margin-top: 20px; padding: 20px;">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6">
+                <h4 class="text-center margin-top-50 editContent">
+                    Footel Left
+                </h4>
+            </div>
+            <div class="col-md-6">
+                <h4 class="text-center margin-top-50 editContent">
+                    Footer Right
+                </h4>
+            </div>
+
+        </div>
+    </div>
+</footer>
